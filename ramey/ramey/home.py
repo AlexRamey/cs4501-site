445f0f69e6e78cd3af3f4333a071332ff228c81e
AlexRamey <alex.b.ramey@gmail.com>
2017-02-12 11:59:45 -0500

    Removed proj1 from version control

--- ramey/ramey/home.py
+++
@@ -1,7 +0,0 @@
-#from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-	helloMsg = "Hello World! Welcome Home And Happy New Year! :)"
-	return render(request, 'ramey/index.html', {'helloMsg' : helloMsg})
-	
