013fd350d9e4476b25254c7d390f42e5e9de096c
AlexRamey <alex.b.ramey@gmail.com>
2017-02-15 16:58:35 -0500

    1. Included fixture with sample data. 2. Added necessary commands to docker-compose to load data into the database from the fixture

--- isa_site/isa_models/models.py
+++ isa_site/isa_models/models.py
@@ -2,23 +2,32 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	email = models.EmailField(max_length=100)
-	password = models.CharField(max_length=100)
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	phone_number = models.CharField(max_length=16)
-	ship_address = models.CharField(max_length=100)
-	ship_city = models.CharField(max_length=100)
-	ship_postal_code = models.CharField(max_length=16)
-	ship_country = models.CharField(max_length=100)
-	buyer_rating = models.FloatField(default=100.0)
-	seller_rating = models.FloatField(default=100.0)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=16)
+    ship_address = models.CharField(max_length=100)
+    ship_city = models.CharField(max_length=100)
+    ship_postal_code = models.CharField(max_length=16)
+    ship_country = models.CharField(max_length=100)
+    buyer_rating = models.FloatField(default=100.0)
+    seller_rating = models.FloatField(default=100.0)
+
+    def __str__(self):
+        return "User: " + self.email
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return "Category: " + self.name
+
 class Condition(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "Condition: " + self.name
 
 class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -30,6 +39,9 @@ class Product(models.Model):
     sold = models.BooleanField()
     condition = models.ForeignKey(Condition, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return "Product: " + self.name
+
 class ProductSnapshot(models.Model):
     seller = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
@@ -37,6 +49,9 @@ class ProductSnapshot(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.FloatField()
     condition = models.ForeignKey(Condition, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "Snapshot: " + self.name
 
 class Order(models.Model):
     product_snapshot = models.ForeignKey(ProductSnapshot, on_delete=models.CASCADE)
@@ -49,3 +64,6 @@ class Order(models.Model):
     completed = models.BooleanField()
     buyer_rating = models.IntegerField(default=5)
     seller_rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return "Order: " + self.product_snapshot.name
