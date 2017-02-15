4f9eeaa1f1d0c11396aafb5b1ae6bc0daf9c6d8e
AlexRamey <alex.b.ramey@gmail.com>
2017-02-15 12:20:03 -0500

    User model

--- isa_site/isa_models/models.py
+++ isa_site/isa_models/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	email = models.EmailField(max_length=100)
+	password = models.CharField(max_length=100)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=16)
+	ship_address = models.CharField(max_length=100)
+	ship_city = models.CharField(max_length=100)
+	ship_postal_code = models.CharField(max_length=16)
+	ship_country = models.CharField(max_length=100)
+	buyer_rating = models.FloatField(default=100.0)
+	seller_rating = models.FloatField(default=100.0)
