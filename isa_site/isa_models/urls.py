32558290429b2bd0aac6c8c8e83be4cf6a692703
edupugantiv <vijayedupuganti@yahoo.com>
2017-02-15 17:47:38 -0500

    added urls

--- isa_site/isa_models/urls.py
+++ isa_site/isa_models/urls.py
@@ -1,9 +1,28 @@
 from django.conf.urls import url
+from .views import users, user, user_orders
+from .views import products, product
+from .views import orders, order
+from .views import productsnapshots, productsnapshot
+from .views import conditions, condition
+from .views import categories, category, category_products
 
 from . import home
 
 urlpatterns = [
     url(r'^$', home.index, name='index'),
 
-    url
+    url(r'^users/$', users),
+    url(r'^users/(?P<user_id>\d+)/$', user),
+    url(r'^users/(?P<buyer_id>\d+)/orders/$', user_orders),
+    url(r'^products/', products),
+    url(r'^products/(?P<product_id>\d+)/$', product),
+    url(r'^orders/$', orders),
+    url(r'^orders/(?P<order_id>\d+)/$', order),
+    url(r'^productsnapshots/', productsnapshots),
+    url(r'^productsnapshots/(?P<productsnapshot_id>\d+)/$', productsnapshot),
+    url(r'^conditions/$', conditions),
+    url(r'^conditions/(?P<condition_id>\d+)/$', condition),
+    url(r'^categories/$', categories),
+    url(r'^categories/(?P<category_id>\d+)/$', category),
+    url(r'^categories/(?P<category_id>\d+)/products/$', category_products),
 ]
--- isa_site/isa_models/views.py
+++ isa_site/isa_models/views.py
@@ -3,71 +3,62 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from isa_models.models import User, Category, Condition, Product, ProductSnapshot, Order
+
+from django.core import serializers 
+import json
 # Create your views here.
 
 def index(request):
     return render(request, 'isa_site/index.html')
 
 def users(request):
-    users = User.objects.all()
-    users_list = list(users)
-    return JsonResponse(users_list, safe = False)
+    return json_response(User.objects.all())
 
 def user(request, user_id):
     user = User.objects.get(pk=user_id)
-    return JsonResponse(user, safe = False)
+    return json_response([user])
 
 def products(request):
-    products = Product.objects.all()
-    products_list = list(products)
-    return JsonResponse(products_list, safe = False)
+    return json_response(Product.objects.all())
 
 def product(request, product_id):
     product = Product.objects.get(pk=product_id)
-    return JsonResponse(product, safe = False)
+    return json_response([product])
 
 def orders(request):
-    orders = Order.objects.all()
-    orders_list = list(orders)
-    return JsonResponse(orders_list, safe = False)
+    return json_response(Order.objects.all())
 
 def order(request, order_id):
     order = Order.objects.get(pk=order_id)
-    return JsonResponse(order, safe = False)
+    return json_response([order])
 
 def productsnapshots(request):
-    productsnapshots = ProductSnapshot.objects.all()
-    productsnapshots_list = list(productsnapshots)
-    return JsonResponse(productsnapshots_list, safe = False)
+    return json_response(ProductSnapshot.objects.all())
 
 def productsnapshot(request, productsnapshot_id):
     productsnapshot = ProductSnapshot.objects.get(pk=productsnapshot_id)
-    return JsonResponse(productsnapshot, safe = False)
+    return json_response([productsnapshot])
 
 def categories(request):
-    categories = Category.objects.all()
-    categories_list = list(categories)
-    return JsonResponse(categories_list, safe = False)
+    return json_response(Category.objects.all())
 
 def category(request, category_id):
     category = Category.objects.get(pk=category_id)
-    return JsonResponse(category, safe = False)
+    return json_response([category])
 
 def conditions(request):
-    conditions = Condition.objects.all()
-    conditions_list = list(conditions)
-    return JsonResponse(conditions_list, safe = False)
+    return json_response(Condition.objects.all())
 
 def condition(request, condition_id):
     condition = Condition.objects.get(pk = condition_id)
-    return JsonResponse(condition, safe = False)
+    return json_response([condition])
 
 def user_orders(request, buyer_id):
-    orders = Order.objects.filter(buyer = buyer_id)
-    orders_list = list(orders)
-    return JsonResponse(orders_list, safe = False)
+    return json_response(Order.objects.all().filter(buyer = buyer_id))
 
 def category_products(request, category_id):
-    products = Product.filter(category = category_id)
-    products_list = list(products)
-    return JsonResponse(products_list, safe = False)
+    return json_response(Product.objects.all().filter(category = category_id))
+
+def json_response(list_of_objects):
+    data = serializers.serialize("json", list_of_objects)
+    return JsonResponse(data, safe = False)
