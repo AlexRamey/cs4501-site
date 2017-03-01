679ec96fa89386c1135a781b8116f72b518a08f5
edupugantiv <vijayedupuganti@yahoo.com>
2017-03-01 13:08:49 -0500

    added top layer

--- isa_web/isa_webapp/views.py
+++ isa_web/isa_webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def base(request):
+	#helloMsg = "Experience Home! API Version: 1"
+	return render(request, 'isa_webapp/base.html')#, {'helloMsg' : helloMsg})
